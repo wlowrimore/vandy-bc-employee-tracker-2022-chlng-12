56ea0de3458d357dd6f7a1648b9f53ff40b19095
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-02 09:10:01 -0600

    adds department call

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,14 +7,27 @@ const app = express();
 
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());000000000
+app.use(express.json());
+
+// Connect to Database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '@Vandy#2022_bc/19!',
+    database: 'employee_tracker'
+  },
+  console.log('Connected to the employee_tracker database.')
+);
+
+db.query(`SELECT * FROM department`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
