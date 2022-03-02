d267a213ce17cbf2771f748595ae657b037b7b69
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-02 09:26:10 -0600

    adds select, delete, insert queryies to department

--- server.js
+++ server.js
@@ -24,6 +24,33 @@ db.query(`SELECT * FROM department`, (err, rows) => {
   console.log(rows);
 });
 
+// GET a single department
+// db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// })
+
+// DELETE a department
+// db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// CREATE a department
+const sql = `INSERT INTO department (id, dep_name) VALUES (?,?)`;
+const params = [1, 'Electric Guitars'];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
