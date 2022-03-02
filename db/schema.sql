979d936c264066508cb61ea94179e82dfa5370d0
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-02 08:23:13 -0600

    adds department table to eployee_tracker(db)

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (dep_name)
+VALUES
+  ('Electric Guitars'),
+  ('Acoustic Guitars'),
+  ('Basses'),
+  ('Effects Pedals'),
+  ('Pickups'),
+  ('Accessories'),
+  ('Internet Sales'),
+  ('Lessons'),
+  ('Repairs'),
+  ('Shipping/Receiving');
